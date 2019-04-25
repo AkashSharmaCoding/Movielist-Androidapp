9dbfaae61682bf90364976449e62f04c45fa9d90
akashsharmacoding <akashwalker7@gmail.com>
2019-04-24 23:33:30 -0400

    first commit

---
+++ Movielist/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace Movielist
+{
+    [Activity(Label = "Movielist", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        EditText movie_name, movie_type;
+        Button add_movie_button;
+        ListView movie_list;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            movie_name = FindViewById<EditText>(Resource.Id.movie_name);
+            movie_type = FindViewById<EditText>(Resource.Id.movie_type);
+
+            add_movie_button = FindViewById<Button>(Resource.Id.add_movie_button);
+
+
+            movie_list = FindViewById<ListView>(Resource.Id.movie_list);
+            List<MovieClass> listMovies = new List<MovieClass>();
+
+            add_movie_button.Click += delegate {
+
+                if (movie_name.Text != "")
+                {
+                    if (movie_type.Text != "")
+                    {
+
+                        MovieClass m1 = new MovieClass();
+                        m1.movie_title = movie_name.Text;
+                        m1.movie_type = movie_type.Text;
+                        listMovies.Add(m1);
+                        var listAdapter = new MovieAdapter(this, listMovies);
+                        movie_list.Adapter = listAdapter;
+
+                        movie_name.Text = "";
+                        movie_type.Text = "";
+                    }
+                    else
+                    {
+                        movie_type.RequestFocus();
+                        movie_type.SetError("required movie type", null);//,Android.Graphics.Drawables.Drawable);
+                    }
+                }
+                else
+                {
+                    movie_name.RequestFocus();
+                    movie_name.SetError("required movie name", null);
+                }
+
+            };
+        }
+    }
+}
+
---
+++ Movielist/MovieAdapter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.Widget;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Graphics.Drawables;
+
+namespace Movielist
+{
+    class MovieAdapter : BaseAdapter
+    {
+        List<MovieClass> listMovies;
+        Activity activity;
+
+        public MovieAdapter(Activity activity, List<MovieClass> list)
+        {
+            this.activity = activity;
+            this.listMovies = list;
+
+        }
+
+        public override int Count
+        {
+            get { return listMovies.Count; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            // could wrap a Contact in a Java.Lang.Object
+            // to return it here if needed
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? activity.LayoutInflater.Inflate(
+                Resource.Layout.row_layout, parent, false);
+            var movieName = view.FindViewById<TextView>(Resource.Id.movie_title_txt);
+            var movieType = view.FindViewById<TextView>(Resource.Id.movie_type_txt);
+            movieName.Text = listMovies[position].movie_title;
+            movieType.Text = listMovies[position].movie_type;
+
+
+            return view;
+        }
+    }
+}
